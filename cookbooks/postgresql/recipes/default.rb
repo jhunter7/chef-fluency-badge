0f46a4e18d83c3bd6a6d1042561d797decd8d795
Jarred <jarredhunter6@gmail.com>
2017-10-31 21:22:40 +0000

    updated postgresql cookbook

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -5,14 +5,14 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 package 'postgresql-server' do
-	notifies :run, 'execute[postregsql-init]'
+	notifies :run, 'execute[postgresql-init]', :immediately
 end
 
-execute 'postregsql-init' do
-	command 'postregsql-setup initdb'
+execute 'postgresql-init' do
+	command 'postgresql-setup initdb'
 	action :nothing
 end
 
-service 'postregsql' do 
+service 'postgresql' do 
 	action [:enable, :start]
 end 
